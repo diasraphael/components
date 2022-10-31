166aa1e441f6d31c5c9144d5dd6cd75bff07586d
diasraphael <diasraphael88@gmail.com>
2022-10-31 19:44:33 +0100

    initial files clean up

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface DropdownProps {
+  dropdownOptions: any;
+}
+
+const Dropdown = (props: DropdownProps) => {
+  const { dropdownOptions } = props;
+  return <div> Dropdown</div>;
+};
+
+export default Dropdown;
